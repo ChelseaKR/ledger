b826c1406bd19c91e83bf782e215e802a2dcffd9
Chelsea Kelly-Reif <3114598+ChelseaKR@users.noreply.github.com>
2026-09-18 12:04:56 -0700

    build: read the region from data.aws_region.region under aws provider 6

--- infra/aws/terraform/iam.tf
+++ infra/aws/terraform/iam.tf
@@ -44,7 +44,7 @@ data "aws_iam_policy_document" "instance" {
     sid     = "DemoSecrets"
     actions = ["ssm:GetParameter", "ssm:PutParameter"]
     resources = [
-      "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter/${var.name_prefix}/*",
+      "arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/${var.name_prefix}/*",
     ]
   }
 
@@ -56,7 +56,7 @@ data "aws_iam_policy_document" "instance" {
     condition {
       test     = "StringEquals"
       variable = "kms:ViaService"
-      values   = ["ssm.${data.aws_region.current.name}.amazonaws.com"]
+      values   = ["ssm.${data.aws_region.current.region}.amazonaws.com"]
     }
   }
 }
